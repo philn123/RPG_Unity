60252d0a34cd2ddabfb3871d6bfa422fc530b772
Phillip <phillipnguyen213@gmail.com>
2020-02-02 13:28:10 -0800

    Video 8 - Integration, added models and small scripts to wear equipment

--- Assets/Scripts/EquipmentManager.cs
+++ Assets/Scripts/EquipmentManager.cs
@@ -14,7 +14,10 @@ public class EquipmentManager : MonoBehaviour
     }
     #endregion
 
+    public Equipment[] defaultItems;
+    public SkinnedMeshRenderer targetMesh;
     Equipment[] currentEquipment;
+    SkinnedMeshRenderer[] currentMeshes;
 
     public delegate void OnEquipmentChanged (Equipment newItem, Equipment oldItem);
     public OnEquipmentChanged onEquipmentChanged;
@@ -26,41 +29,59 @@ public class EquipmentManager : MonoBehaviour
 
         int numSlots = System.Enum.GetNames(typeof(EquipmentSlot)).Length;
         currentEquipment = new Equipment[numSlots];
+        currentMeshes = new SkinnedMeshRenderer[numSlots];
+
+        EquipDefaultItems();
     }
 
     public void Equip (Equipment newItem)
     {
         int slotIndex = (int)newItem.equipSlot;
+        Equipment oldItem = Unequip(slotIndex);;
 
-        Equipment oldItem = null;
-
-        if(currentEquipment[slotIndex] != null)
-        {
-            oldItem = currentEquipment[slotIndex];
-            inventory.Add(oldItem);
-        }
+        // if(currentEquipment[slotIndex] != null)
+        // {
+        //     oldItem = currentEquipment[slotIndex];
+        //     inventory.Add(oldItem);
+        // }
 
         if(onEquipmentChanged != null){
-            onEquipmentChanged.invoke(newItem, oldItem);
+            onEquipmentChanged.Invoke(newItem,null);
         }
 
+        SetEquipmentBlendShapes(newItem, 100);
+
         currentEquipment[slotIndex] = newItem;
+        SkinnedMeshRenderer newMesh = Instantiate<SkinnedMeshRenderer>(newItem.mesh);
+        newMesh.transform.parent = targetMesh.transform;
+
+        newMesh.bones = targetMesh.bones;
+        newMesh.rootBone = targetMesh.rootBone;
+        currentMeshes[slotIndex] = newMesh;
 
     }
 
-    public void Unequip (int slotIndex)
+    public Equipment Unequip (int slotIndex)
     {
         if(currentEquipment[slotIndex] != null)
         {
+            if(currentMeshes[slotIndex] != null)
+            {
+                Destroy(currentMeshes[slotIndex].gameObject);
+            }
+
             Equipment oldItem = currentEquipment[slotIndex];
+            SetEquipmentBlendShapes(oldItem, 0);
             inventory.Add(oldItem);
 
             currentEquipment[slotIndex] = null;
 
             if(onEquipmentChanged != null){
-                onEquipmentChanged.invoke(null, oldItem);
+                onEquipmentChanged.Invoke(null, oldItem);
             }
+            return oldItem;
         }
+        return null;
     }
 
     public void UnequipAll () 
@@ -68,6 +89,7 @@ public class EquipmentManager : MonoBehaviour
         for(int i = 0; i < currentEquipment.Length; ++i){
             Unequip(i);
         }
+        EquipDefaultItems();
 
     }
 
@@ -76,6 +98,24 @@ public class EquipmentManager : MonoBehaviour
         if(Input.GetKeyDown(KeyCode.U)){
             UnequipAll();
         }
+
+    }
+
+    void EquipDefaultItems ()
+    {
+        foreach (Equipment item in defaultItems)
+        {
+            Equip(item);
+        }
+    }
+
+    void SetEquipmentBlendShapes(Equipment item, int weight)
+    {
+        foreach (EquipmentMeshRegion blendShape in item.coveredMeshRegions)
+        {
+            targetMesh.SetBlendShapeWeight((int)blendShape, weight);
+        }
+
 
     }
 
--- Assets/Scripts/Items/Equipment.cs
+++ Assets/Scripts/Items/Equipment.cs
@@ -6,6 +6,8 @@ using UnityEngine;
 public class Equipment : Item
 {
     public EquipmentSlot equipSlot;
+    public SkinnedMeshRenderer mesh;
+    public EquipmentMeshRegion[] coveredMeshRegions;
 
     public int armorModifier;
     public int damageModifier;
@@ -23,3 +25,4 @@ public class Equipment : Item
 }
 
 public enum EquipmentSlot {Head, Chest, Legs, Weapon, Shield, Feet}
+public enum EquipmentMeshRegion {Legs, Arms, Torso}; //Corresponds to body blend shapes
